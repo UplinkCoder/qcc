51597fb884e835f40a5876ff63a055fb13674f2e
Stefan Koch <uplink.coder@googlemail.com>
2014-12-20 17:12:37 +0000

    huge changes to the parser

---
+++ parsetree.d
@@ -0,0 +1,33 @@
+﻿module qcc.parsetree;
+import qcc.token;
+
+abstract class Node {}
+
+abstract class Expression : Node {}
+
+abstract class Statement : Node {}
+
+abstract class Declaration : Node {}
+
+abstract class Annotation : Node {}
+
+class VaraibleDeclaration : Declaration {
+	Token type;
+	Token name;
+}
+
+class FunctionDeclaration : Declaration {
+	Token return_type;
+	Token function_name;
+	VaraibleDeclaration[] parameters;
+}
+
+class Block : Statement {
+	Statement[] statements;
+}
+
+class FunctionDefinition : Statement {
+	FunctionDeclaration function_decl;
+	Block function_body;
+	
+}
